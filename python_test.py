48b44f8c1b38c3cf5d2deb47abc8b7037fa92eee
haley <hkarp2000@gmail.com>
2023-06-07 09:53:17 -0400

    added to the file

--- python_test.py
+++ python_test.py
@@ -1,1 +1,9 @@
-print("Hello Haley")
+import os
+
+path = (r"/Users/haley.karp/Desktop/vscode/socialcyber/Reachability")
+
+dirp_list = []
+for dirpath, subdirs, files in os.walk(path):
+    dirp_list.append(dirpath)
+
+print(dirp_list)
